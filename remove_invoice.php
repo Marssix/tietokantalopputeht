f76362ca6c3b444818375267c3c4f95575a6d296
Marssix <68240251+Marssix@users.noreply.github.com>
2023-04-21 17:42:47 +0300

    Update remove_invoice.php

--- remove_invoice.php
+++ remove_invoice.php
@@ -4,12 +4,21 @@ $dbcon = createDbConnection();
 
 if (isset($_GET['invoice_id'])) {
     $invoice_id = $_GET['invoice_id'];
-
+    
     $stmt = $dbcon->prepare("DELETE FROM invoice_items WHERE InvoiceId = ?");
     $stmt->execute([$invoice_id]);
 
     echo "Poistettu onnistuneesti.";
+    
+} elseif (isset($_GET['invoice_item_id'])) {
+    $invoice_item_id = $_GET['invoice_item_id'];
+    
+    $stmt = $dbcon->prepare("DELETE FROM invoice_items WHERE InvoiceItemId = ?");
+    $stmt->execute([$invoice_item_id]);
+    
+    echo "Poistettu onnistuneesti.";
+    
 } else {
-    echo "invoice_id ei ole annettu.";
+    echo "invoice_id tai invoice_item_id ei ole annettu.";
 }
 ?>
